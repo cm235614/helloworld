317fed74d67cc0028a3291b64e1c5e112a9c9c0b
cm235614 <2263306252@qq.com>
2019-05-27 15:10:09 +0800

    helloworld提交

---
+++ HelloWorld/src/com/byzx/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.byzx.helloworld;
+
+public class HelloWorld {
+/**@文件名: HelloWorld.java
+ * @类功能说明: 
+ * @作者: GuoHaiFeng
+ * @Email: 1191619897@qq.com
+ * @日期: 2019年5月27日下午2:13:14
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: GuoHaiFeng</li> 
+ * 	 <li>日期: 2019年5月27日下午2:13:14</li> 
+ *	 <li>内容: </li>
+ * </pre>
+
+*/
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
